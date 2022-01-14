86d1f821f966f80460249b25f9214b92ebf12296
Brendan Wilson <31160104+Brendan-A-Wilson@users.noreply.github.com>
2022-01-14 16:23:57 +0000

    Add files via upload

--- My Quiz/end.js
+++ My Quiz/end.js
@@ -30,5 +30,5 @@ saveHighScore = e => {
     highScores.splice(5)
 
     localStorage.setItem("highScores", JSON.stringify(highScores))
-    window.location.assign('http://127.0.0.1:5500/My%20Quiz/index.html')
+    window.location.assign('./index.html')
 }
